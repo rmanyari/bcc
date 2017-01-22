bda9ec24dac07a87f5ca894d511cdd1778f2793c
Rodrigo Manyari <rodrigo.manyari@hotmail.com>
2017-01-21 21:06:07 -0500

    Adding ebpf functions impl

--- src/nodejs/bcc/index.js
+++ src/nodejs/bcc/index.js
@@ -32,6 +32,65 @@ class BCCProgram {
         return this.program.destroy();
     }
 
+    license() {
+        return this.program.license();
+    }
+
+    kernelVersion() {
+        return this.program.kernelVersion();
+    }
+
+    nbFunctions() {
+        return this.program.nbFunctions();
+    }
+
+    functionName(id) {
+        this._checkId(id);
+        return this.program.functionName(id);
+    }
+
+    startFunctionById(id) {
+        this._checkId(id);
+        return this.program.startFunctionById(id);
+    }
+
+    startFunctionByName(name) {
+        this._checkName(name);
+        return this.program.startFunctionByName(name);
+    }
+
+    functionSizeById(id) {
+        this._checkId(id);
+        return this.program.functionSizeById(id);
+    }
+
+    functionSizeByName(name) {
+        this._checkName(name);
+        return this.program.functionSizeByName(name);
+    }
+
+    // Verifies the function id is valid.
+    _checkId(id) {
+        if (!_.isInteger(id)) {
+            throw new Error('ID must be an integer');
+        }
+        const nbFns = this.nbFunctions() - 1;
+        if(id > nbFns) {
+            throw new Error(`ID ${id} is invalid. The program contains only ${nbFns} function${nbFns > 1 ? 's' : ''}`);
+        }
+    }
+
+    // Verifies the function name is valid.
+    _checkName(name) {
+        if (!_.isString(name)) {
+            throw new Error('Function name must be a string');
+        }
+        const names = _.map(_.range(0, this.nbFunctions()), (id) => this.functionName(id));
+        if (!_.includes(names, name)) {
+            throw new Error(`Function name ${name} is invalid. Valid function names are ${JSON.stringify(names)}`);
+        }
+    }
+
 }
 
 module.exports = BCCProgram;
--- src/nodejs/bcc/test/testIndex.js
+++ src/nodejs/bcc/test/testIndex.js
@@ -1,7 +1,7 @@
 const test = require('tape');
 const BCCProgram = require('../index');
 
-const VALID_PROGRAM_FILE = '../../examples/networking/distributed_bridge/tunnel.c';
+const VALID_PROGRAM_FILE = '../../examples/networking/http_filter/http-parse-complete.c';
 const INVALID_PROGRAM_FILE = 'i_dont_exist.c';
 const INVALID_PROGRAM_FOLDER = '/etc';
 
@@ -39,5 +39,91 @@ test('BCCProgram constructor should validate cFlags is an array of strings', (t)
     t.throws(() => {
         const program = new BCCProgram(VALID_PROGRAM_FILE, 0, [0]);
     }, /cFlags/);
+    t.end();
+});
+
+test('license fn returns GPL', (t) => {
+    const program = new BCCProgram(VALID_PROGRAM_FILE, 0, []);
+    t.equals(program.license(), 'GPL');
+    t.end();
+});
+
+test('kernelVersion fn returns the kernel version', (t) => {
+    const program = new BCCProgram(VALID_PROGRAM_FILE, 0, []);
+    // TODO: Improve this assertion, how to validate that the 
+    //       number returned is correct?
+    t.ok(program.kernelVersion() !== 0);
+    t.end();
+});
+
+test('nbFunctions fn should return the correct nb of functions in the ebpf program', (t) => {
+    const program = new BCCProgram(VALID_PROGRAM_FILE, 0, []);
+    t.ok(program.nbFunctions() === 1);
+    t.end();
+});
+
+test('functionName fn should return a fn name given a valid ID', (t) => {
+    const program = new BCCProgram(VALID_PROGRAM_FILE, 0, []);
+    t.ok(program.functionName(0) === 'http_filter');
+    t.end();
+});
+
+test('functionName fn should validate ID is an integer', (t) => {
+    const program = new BCCProgram(VALID_PROGRAM_FILE, 0, []);
+    t.throws(() => {
+        program.functionName('bad_input');
+    }, /must be an integer/);
+    t.end();
+});
+
+test('functionName fn should validate ID is within a valid range', (t) => {
+    const program = new BCCProgram(VALID_PROGRAM_FILE, 0, []);
+    t.throws(() => {
+        program.functionName(1);
+    }, /program contains only/);
+    t.end();
+});
+
+test('startFunctionById fn should start a function given a valid id', (t) => {
+    const program = new BCCProgram(VALID_PROGRAM_FILE, 0, []);
+    // TODO: Enhance assertion. attach to socket and make sure that data is written
+    //       to the tables
+    program.startFunctionById(0);
+    t.end();
+});
+
+test('startFunctionByName fn should start a function given a valid name', (t) => {
+    const program = new BCCProgram(VALID_PROGRAM_FILE, 0, []);
+    // TODO: Enhance assertion. attach to socket and make sure that data is written
+    //       to the tables
+    program.startFunctionByName('http_filter');
+    t.end();
+});
+
+test('startFunctionByName fn should validate that the name is valid', (t) => {
+    const program = new BCCProgram(VALID_PROGRAM_FILE, 0, []);
+    t.throws(() => {
+        program.startFunctionByName('invalid_fn');
+    }, /Valid function names are/);
+    t.end();
+});
+
+test('startFunctionByName fn should validate that the name is a string', (t) => {
+    const program = new BCCProgram(VALID_PROGRAM_FILE, 0, []);
+    t.throws(() => {
+        program.startFunctionByName([]);
+    }, /name must be a string/);
+    t.end();
+});
+
+test('functionSizeById fn should be greater than 0 for a valid fn/program', (t) => {
+    const program = new BCCProgram(VALID_PROGRAM_FILE, 0, []);
+    t.ok(program.functionSizeById(0) > 0);
+    t.end();
+});
+
+test('functionSizeByName fn should match the size of the same fn retreived by id', (t) => {
+    const program = new BCCProgram(VALID_PROGRAM_FILE, 0, []);
+    t.equals(program.functionSizeById(0), program.functionSizeByName('http_filter'));
     t.end();
 });
